65e1430f3949c676c8e974bdceaf8067358c6743
Ubuntu Eze <ezeosy.io@gmail.com>
2018-06-21 06:58:30 +0100

    created a new spring boot application

---
+++ src/main/java/com/message/app/messageapi/MessageApiApplication.java
@@ -0,0 +1,12 @@
+package com.message.app.messageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApiApplication.class, args);
+    }
+}
